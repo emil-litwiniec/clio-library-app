67cacb19bf41706489140206ab6c007135ba60e6
Emil <emilitwiniec@gmail.com>
2019-06-02 07:42:16 +0200

    Refactor db.js, manage table with one function.

--- db.js
+++ db.js
@@ -11,76 +11,50 @@ pool.on('connect', () => {
     console.log('connected to the clio_db');
 })
 
-const createUsersTable = () => {
-    const query =
-    `CREATE TABLE IF NOT EXISTS
-        users(
-            id UUID PRIMARY KEY,
-            name VARCHAR(30) NOT NULL,
-            surname VARCHAR(30) NOT NULL,
-            email VARCHAR(128) UNIQUE NOT NULL,
-            password VARCHAR(128) NOT NULL,
-            phone_number VARCHAR(30) NOT NULL
-        )`;
 
-    pool.query(query).then((data) => {
-        console.log(data);
-        console.log('users table created');
-        pool.end();
-    }).catch(err => {
-        console.log(err);
-        console.log('unable to create users table');
-        pool.end();
-    })
-};
-
-const createAdminsTable = () => {
-    const query = 
-    `CREATE TABLE IF NOT EXISTS
-        admins(
-            user_id UUID PRIMARY KEY,
-            FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-        )`;
-
+const table = (table, query) => {
     pool.query(query).then(data => {
         console.log(data);
-        console.log('admins table created');
+        console.log(`${table} table created`);
         pool.end();
     }).catch(err => {
         console.log(err);
-        console.log('unable to create admins table');
-        pool.end();
+        console.log(`unable to create ${table} table`)
     })
+}
 
-};
+const Queries = {
+    create: {users: `CREATE TABLE IF NOT EXISTS
+    users(
+        id UUID PRIMARY KEY,
+        name VARCHAR(30) NOT NULL,
+        surname VARCHAR(30) NOT NULL,
+        email VARCHAR(128) UNIQUE NOT NULL,
+        password VARCHAR(128) NOT NULL,
+        phone_number VARCHAR(30) NOT NULL
+    )`,
+    admins: `CREATE TABLE IF NOT EXISTS
+    admins(
+        user_id UUID PRIMARY KEY,
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    )`,},
 
-const dropUsersTable = () => {
-    const query = 'DROP TABLE IF EXISTS users RETURNNING *';
+    drop: {
+        users: 'DROP TABLE IF EXISTS users RETURNNING *',
+        admins: 'DROP TABLE IF EXISTS admins RETURNING *'
+    }
 
-    pool.query(query).then(data => {
-        console.log(data);
-        console.log('users table has been dropped');
-        pool.end();
-    }).catch(err => {
-        console.log(err);
-        console.log('unable to drop users table');
-        pool.end();
-    }) ;
-};
+}
 
-const dropAdminsTable = () => {
-    const query = 'DROP TABLE IF EXISTS admins RETURNING *';
+const createUsersTable = table('users', Queries.create.users);
 
-    pool.query(query).then(data => {
-        console.log(data);
-        console.log('admins table has been dropped');
-        pool.end();
-    }).catch(err => {
-        console.log(err);
-        console.log('unable to drop admins table');
-        pool.end();
-    })
-};
+const createAdminsTable = table('admins', Queries.create.admins);
+
+const dropUsersTable = table('users', Queries.drop.users);
+
+const dropAdminsTable = table('admins', Queries.drop.admins);
+
+
 
 const createAllTables = () => {
     createUsersTable();
