419e53012ff4f3e62edfa5c7f7d6f81bb506ad80
Emil Litwiniec <emilitwiniec@gmail.com>
2019-06-05 00:13:14 +0200

    Refactor Books.insert. Check if author exists.

--- src/controllers/Books.js
+++ src/controllers/Books.js
@@ -7,35 +7,86 @@ import db from "../db/index";
 const Books = {
     async insert(req, res) {
 
-        if (!req.body.title || !req.body.authorId || !req.body.lang || !req.body.isbn) {
+        if (!req.body.title || !req.body.authorFirst || !req.body.authorLast || !req.body.lang || !req.body.isbn) {
             return res.status(400).send({ "message": "Please, provide all required values..." });
         };
 
-        const query = `INSERT INTO
-        books(book_id, title, author_id, series, edition, genre_id, 
-            keywords, ukd, lang, pub_year, translator_id, pub_id, isbn, create_date)
-        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
-        returning *`;
+        const searchAuthorQuery = `SELECT author_id
+        FROM authors
+        WHERE LOWER(first_name) = '${req.body.authorFirst.toLowerCase()}'
+        AND
+            LOWER(last_name) = '${req.body.authorLast.toLowerCase()}'`;;
 
-        const values = [
-            uuidv4(),
-            req.body.title,
-            req.body.authorId,
-            req.body.series,
-            req.body.edition,
-            req.body.genreId,
-            req.body.keywords,
-            req.body.ukd,
-            req.body.lang,
-            req.body.pub_year,
-            req.body.translator_id,
-            req.body.pub_id,
-            req.body.isbn,
-            moment(new Date())
-        ];
+            
+            try {
+                const { rows: authorId } = await db.query(searchAuthorQuery);
 
-        try {
-            const { rows } = await db.query(query, values);
+            
+                const insertQuery = `INSERT INTO
+                books(book_id, title, author_id, series, edition, genre_id, 
+                    keywords, ukd, lang, pub_year, translator_id, pub_id, isbn, create_date)
+                VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+                returning *`;
+        
+                
+
+            if(!authorId[0]) {
+                const query = `INSERT INTO
+                authors(first_name, last_name, origin)
+                VALUES($1, $2, $3)
+                RETURNING *`;
+
+                const values = [
+                    req.body.authorFirst,
+                    req.body.authorLast,
+                    req.body.authorOrigin
+                ];
+
+                const authorResponse = await db.query(query, values);
+
+                if(!authorResponse.rows[0]) {
+                    return res.status(400).send({"message": "Something went wrong with adding author to the database."})
+                }
+
+                const insertValuesAlt = [
+                    uuidv4(),
+                    req.body.title,
+                    authorResponse.rows[0].author_id,
+                    req.body.series,
+                    req.body.edition,
+                    req.body.genreId,
+                    req.body.keywords,
+                    req.body.ukd,
+                    req.body.lang,
+                    req.body.pub_year,
+                    req.body.translator_id,
+                    req.body.pub_id,
+                    req.body.isbn,
+                    moment(new Date())
+                ];
+
+                
+                const { rows } = await db.query(insertQuery, insertValuesAlt);
+                return res.status(201).send(rows[0]);
+
+            }
+            const insertValues = [
+                uuidv4(),
+                req.body.title,
+                authorId[0].author_id,
+                req.body.series,
+                req.body.edition,
+                req.body.genreId,
+                req.body.keywords,
+                req.body.ukd,
+                req.body.lang,
+                req.body.pub_year,
+                req.body.translator_id,
+                req.body.pub_id,
+                req.body.isbn,
+                moment(new Date())
+            ];
+            const { rows } = await db.query(insertQuery, insertValues);
             return res.status(201).send(rows[0]);
         } catch (err) {
             return res.status(400).send(err);
@@ -44,7 +95,7 @@ const Books = {
 
     async remove(req, res) {
         if(!req.body.bookId) {
-            return res.status(400).send({'message': "Provide id of the book to the delete"});
+            return res.status(400).send({'message': "Provide id of the book to delete"});
         }
 
         const query = `DELETE
