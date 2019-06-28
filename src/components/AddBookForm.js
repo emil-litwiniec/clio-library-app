533d5627726e4d8a93ff8e3b3d67ba4a62cd967e
Emil Litwiniec <emilitwiniec@gmail.com>
2019-06-28 11:15:21 +0200

    Add initial add book form for admin page. Connect with API.

---
+++ src/components/AddBookForm.js
@@ -0,0 +1,208 @@
+import React from "react";
+import {Formik} from 'formik';
+import axios from 'axios'
+import Select from "../components/Select";
+
+
+// TODO => place this function from some helper object outside
+const convertToSelectOptions = (arr) => {
+    // tranform response data array of objects to meet
+    // with input crieteria of Select component
+    if (Array.isArray(arr)){
+        return  arr.map(obj => ({ 
+             value: obj.pub_id,
+             label: obj.name,
+             name: obj.name }))
+    } else {
+        return []
+    }
+};
+
+class AddBookForm extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {publishers: {}}
+    }
+
+    componentDidMount() {
+
+        axios({
+            method: "GET",
+            url: "http://localhost:3000/getAllPubs"
+        })
+        .then((res) => {
+            this.setState(state => {
+                return {publishers: res.data}
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Formik
+                    enableReinitialize
+                    initialValues={{
+                        title: "",
+                        authorFirst: "",
+                        authorLast: "",
+                        pubYear: "",
+                        pubId: "",
+                        series: "",
+                        edition: "",
+                        genreId: "",
+                        lang: "",
+                        translatorId: "",
+                        isbn: "",
+                        keywords: "",
+                        ukd: ""
+                    }}
+                    onSubmit={(values, actions) => {
+                        this.props.handleSubmit(values);
+                        actions.setSubmitting(false);
+                    }}
+                    render={props => (
+                        // TODO => form validation, mandatory fields
+                        <form onSubmit={props.handleSubmit}>
+                            <label>Title:</label>
+                            <input
+                                type="text"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.title}
+                                name="title"
+                            />
+                            {props.errors.name && (
+                                <div id="feedback">
+                                    {props.errors.name}
+                                </div>
+                            )}
+                            <label>Author first name:</label>
+                            <input
+                                type="text"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.authorFirst}
+                                name="authorFirst"
+                            />
+
+                            <label>Author last name:</label>
+                            <input
+                                type="text"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.authorLast}
+                                name="authorLast"
+                            />
+
+                            <label>Publication year:</label>
+                            <input
+                                type="text"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.pubYear}
+                                name="pubYear"
+                            />
+                            <label>Series:</label>
+                            <input
+                                type="text"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.series}
+                                name="series"
+                            />
+                            <label>Edition:</label>
+                            <input
+                                type="text"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.edition}
+                                name="edition"
+                            />
+                            <label>Isbn:</label>
+                            <input
+                                type="text"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.isbn}
+                                name="isbn"
+                            />
+
+                            {/* <label>Keywords:</label>
+                            <input
+                                type="text"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.keywords}
+                                name="isbn"
+                                disabled={true}
+                            />
+
+                            <label>Translator:</label>
+                            <input
+                                type="text"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.translatorId}
+                                name="isbn"
+                                disabled={true}
+                            /> */}
+
+
+// UKD change type in SQL Database
+                            <label>UKD:</label>
+                            <input
+                                type="text"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.ukd}
+                                name="ukd"
+                            />
+
+                            <label>Language:</label>
+                            <input
+                                type="text"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.lang}
+                                name="lang"
+                            />
+
+                            {/* <Select
+                                label="Genre:"
+                                name="genreId"
+                                value="genreId"
+                                options={
+                                    this.state.genres &&
+                                    convertToSelectOptions(
+                                        this.state.genres
+                                    )
+                                }
+                                formikProps={props}
+                            /> */}
+
+                            <Select
+                                label="Publisher:"
+                                name="pubId"
+                                value="pubId"
+                                options={
+                                    this.state.publishers &&
+                                    convertToSelectOptions(
+                                        this.state.publishers
+                                    )
+                                }
+                                formikProps={props}
+                            />
+
+                            <button type="submit">Submit</button>
+                        </form>
+                    )}
+                />
+            </div>
+        );
+    }
+}
+
+
+export default AddBookForm;
---
+++ src/components/UserPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserPage = () => {
+    return (
+        <div>
+            <h1>
+                This is user page!
+            </h1>
+        </div>
+    )
+}
+
+export default UserPage;
