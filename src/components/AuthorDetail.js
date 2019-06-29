97dc72ee7cd2b3e2158c5add9693a1a7b96f9d1c
Emil Litwiniec <emilitwiniec@gmail.com>
2019-06-29 14:46:14 +0200

    Add AuthorDetail component, creater route

---
+++ src/components/AuthorDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const AuthorDetail = (props) => {
+    return (
+        <div>
+            <p>{JSON.stringify(props)}</p>
+        </div>
+    )
+}
+
+
+
+export default AuthorDetail;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,33 +1,25 @@
 import React from "react";
 import {Router, Route, Switch } from "react-router-dom";
-// import { createBrowserHistory } from "history";
 import { createBrowserHistory } from 'history';
 
-
+import AuthorDetailPage from "../components/AuthorDetailPage";
 import MainPage from "../components/MainPage.js";
 import ResultDetailPage from "../components/ResultDetailPage.js";
+import AdminPanelPage from "../components/AdminPanelPage";
+import UserPage from "../components/UserPage";
 
 export const history = createBrowserHistory();
 
 const AppRouter = () => (
     <Router history={history}>
         <div>
-            {/* <Switch>
-                <PublicRoute exact path="/" component={LoginPage} />
-                <PrivateRoute
-                    path="/dashboard"
-                    component={DashboardPage}
-                />
-
-                <Route component={NotFoundPage} />
-            </Switch> */}
-
-        <Switch>
-            <Route exact path='/' component={MainPage} />
-            <Route path='/result/:bookId' component={ResultDetailPage}/>
-
-
-        </Switch>
+            <Switch>
+                <Route exact path="/" component={MainPage} />
+                <Route path="/result/:id" component={ResultDetailPage} />
+                <Route path="/author/:authorId" component={AuthorDetailPage} />
+                <Route path="/admin" component={AdminPanelPage} />
+                <Route path="/user" component={UserPage} />
+            </Switch>
         </div>
     </Router>
 );
