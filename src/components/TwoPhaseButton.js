fb522cb05cc7586704d3e5894175c581a0c511ad
Emil Litwiniec <emilitwiniec@gmail.com>
2019-07-04 13:18:13 +0200

    Add universal two-phase button component.

---
+++ src/components/TwoPhaseButton.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+
+const TwoPhaseButton = ({
+    handleSubmit,
+    id,
+    btnName,
+    confirmMessage = "Are you sure"
+}) => {
+    const [phase, setPhase] = useState(1);
+
+    return (
+        <>
+            {phase == 1 && <button onClick={() => setPhase(2)}>{btnName}</button>}
+
+            {phase == 2 && (
+                <div>
+                    <p>{confirmMessage}</p>
+                    <button onClick={() => {
+                        handleSubmit(id);
+                        setPhase(1);
+                        }}>Yes</button>
+                    <button onClick={() => setPhase(1)}>No</button>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default TwoPhaseButton;
