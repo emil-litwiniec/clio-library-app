3b9ad2f73012cc8e4d3f03949cae4633c94a4e02
Emil Litwiniec <emilitwiniec@gmail.com>
2019-07-14 14:40:49 +0200

    Add ThemeProvider wrapper

--- src/app.js
+++ src/app.js
@@ -4,16 +4,27 @@ import ReactDOM from "react-dom";
 import "./styles/styles.scss";
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme } from "@material-ui/core/styles"
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+
+
+const theme = createMuiTheme({
+
+})
 
 
 const store = configureStore();
 
-import LoadingPage from "./components/LoadingPage";
 import AppRouter from "./routers/AppRouter";
 
 const jsx = (
         <Provider store={store}>
+                <ThemeProvider theme={theme}>
+                        <CssBaseline />
                         < AppRouter/>
+                </ThemeProvider>
         </Provider>
 );
 
