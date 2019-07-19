53fc3556434eb61c57b420cc89b577b95d1664ac
Emil Litwiniec <emilitwiniec@gmail.com>
2019-07-19 17:27:10 +0200

    UserBorrowsHistory mui styling with expansion panels.

--- src/components/UserBorrowsHistory.js
+++ src/components/UserBorrowsHistory.js
@@ -1,45 +1,241 @@
 import React from 'react';
+import moment from 'moment';
+import { Link as RouterLink } from 'react-router-dom';
 
 import {
+  Link,
+  Grid,
   Box,
-  Divider,
-  Typography,
-  Paper,
   List,
-  ListItem
+  ListItem,
+  Typography,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails
 } from '@material-ui/core';
 
-const UserBorrows = ({ borrowsHistory }) => {
-  return (
-    <>
-      <Typography> User borrows history: </Typography>
-      <Paper>
-        <List>
-          {borrowsHistory.map((borrow, idx) => {
-            return (
-              <Box key={idx}>
-                <ListItem>
-                  <Box>
-                    <Typography>Title: {borrow.title}</Typography>
-                    <Typography>Author: {borrow.author}</Typography>
-                    <Typography>Pubication year: {borrow.pub_year}</Typography>
-                    <Typography>Isbn: {borrow.isbn}</Typography>
+import { LibraryBooksOutlined as Book, ExpandMore } from '@material-ui/icons';
 
-                    <Typography>Taken date: {borrow.taken_date}</Typography>
-                    <Typography>
-                      Expected brought date: {borrow.exp_brought_date}
-                    </Typography>
-                    <Typography>brought date: {borrow.brought_date}</Typography>
-                    <Typography>Prolongs: {borrow.prolongs}</Typography>
-                  </Box>
-                </ListItem>
-                <Divider />
-              </Box>
-            );
-          })}
-        </List>
-      </Paper>
-    </>
+import { makeStyles } from '@material-ui/styles';
+
+moment.locale('en-gb');
+
+const formatDate = date => {
+  return moment(date).format('L');
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360
+  },
+  listRoot: {
+    paddingTop: 0,
+    paddingBottom: 0
+  },
+  listItemRoot: {
+    paddingTop: 15,
+    paddingBottom: 15
+  },
+  boxText: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  boxWrapper: {
+    alignItems: 'baseline'
+  },
+  boxWrapperText: {
+    display: 'flex'
+  },
+  mainBoxWrapper: {
+    alignItems: 'center',
+    width: '100%'
+  },
+  typoElement: {
+    marginRight: theme.spacing(2),
+    fontSize: '.8rem'
+  },
+  typoElementTitle: {
+    marginRight: theme.spacing(2),
+    fontSize: '1rem'
+  },
+  typoElementRight: {
+    marginLeft: 'auto'
+  },
+  span: {
+    fontSize: '.8rem',
+    marginRight: theme.spacing(1)
+  },
+  buttonWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    [theme.breakpoints.only('xs')]: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginTop: theme.spacing(2)
+    }
+  },
+  number: {
+    fontSize: '.8rem'
+  },
+  expansionPanelSummary: {
+    alignItems: 'center'
+  },
+  expansionPanelSummaryRoot: {
+    padding: '0 15px 0 15px'
+  },
+  expansionPanelRoot: {
+    backgroundColor: '#fbfbfb'
+  }
+}));
+
+const UserBorrows = ({ borrowsHistory }) => {
+  const classes = useStyles();
+  return (
+    <Box mt={4}>
+      <Typography variant="overline"> Borrows history: </Typography>
+      <List>
+        {borrowsHistory.map((borrow, idx) => {
+          return (
+            <ExpansionPanel
+              classes={{
+                root: classes.expansionPanelRoot
+              }}
+            >
+              <ExpansionPanelSummary
+                classes={{
+                  content: classes.expansionPanelSummary,
+                  root: classes.expansionPanelSummaryRoot
+                }}
+                expandIcon={<ExpandMore />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography
+                  variant="body1"
+                  className={classes.typoElement}
+                  classes={{
+                    body1: classes.number
+                  }}
+                >
+                  {idx + 1}.
+                </Typography>
+
+                <Box>
+                  <Link
+                    component={RouterLink}
+                    to={`/result/${borrow.book_id}`}
+                    variant="h6"
+                    className={classes.typoElementTitle}
+                  >
+                    {borrow.title}
+                  </Link>
+                  <Typography variant="body1" className={classes.typoElement}>
+                    <span className={classes.span}>by</span>
+                    {borrow.author}
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography className={classes.span}>
+                    Brought date:
+                  </Typography>
+                  <Typography variant="body1" className={classes.typoElement}>
+                    {formatDate(borrow.brought_date)}
+                  </Typography>
+                </Box>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Box key={idx}>
+                  <ListItem
+                    classes={{
+                      root: classes.listItemRoot
+                    }}
+                    key={idx}
+                  >
+                    <Grid
+                      container
+                      display="flex"
+                      className={classes.mainBoxWrapper}
+                    >
+                      <Grid item sm={2}>
+                        <Book />
+                      </Grid>
+
+                      <Grid
+                        item
+                        container
+                        display="flex"
+                        className={classes.boxWrapper}
+                        sm={10}
+                      >
+                        <Grid
+                          item
+                          container
+                          className={classes.boxWrapperText}
+                          sm={10}
+                          xs={12}
+                        >
+                          <Grid item className={classes.boxText} sm={6} xs={12}>
+                            <Link
+                              component={RouterLink}
+                              to={`/result/${borrow.book_id}`}
+                              variant="h6"
+                              className={classes.typoElement}
+                            >
+                              {borrow.title}
+                            </Link>
+                            <Typography
+                              variant="body1"
+                              className={classes.typoElement}
+                            >
+                              <span className={classes.span}>by</span>
+                              {borrow.author}
+                            </Typography>
+                            <Typography
+                              variant="body1"
+                              className={classes.typoElement}
+                            >
+                              <span className={classes.span}>isbn: </span>
+                              {borrow.isbn}
+                            </Typography>
+                          </Grid>
+                          <Grid item className={classes.boxText} sm={6} xs={12}>
+                            <Typography
+                              variant="body1"
+                              className={classes.typoElement}
+                            >
+                              <span className={classes.span}>Taken date: </span>
+                              {formatDate(borrow.taken_date)}
+                            </Typography>
+                            <Typography
+                              variant="body1"
+                              className={classes.typoElement}
+                            >
+                              <span className={classes.span}>
+                                Brought date:
+                              </span>
+                              {formatDate(borrow.brought_date)}
+                            </Typography>
+
+                            <Typography
+                              variant="body1"
+                              className={classes.typoElement}
+                            >
+                              <span className={classes.span}>Prolongs: </span>
+                              {borrow.prolongs}
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </ListItem>
+                </Box>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          );
+        })}
+      </List>
+    </Box>
   );
 };
 
